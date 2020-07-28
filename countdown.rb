5613cc4248471a2b6a988e1b6765603784e89f2d
Tyler Huffman <ty.huff24@gmail.com>
2020-07-28 14:50:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,12 +2,12 @@
 
 def countdown(n)
   while n >= 0
-  if n == 1
-    puts "#{n} SECOND!"
-  else
-    puts "#{n} SECONDS!"
-    n -= 1
+    if n == 1
+      puts "#{n} SECOND!"
+    else
+      puts "#{n} SECONDS!"
     end
+    n -= 1
   end
-    puts "HAPPY NEW YEAR!"
+    HAPPY NEW YEAR!
 end
